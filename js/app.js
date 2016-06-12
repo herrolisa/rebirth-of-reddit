5fe3effd08426530fc3422e44bf4ef4061a7022a
Lisa <misslisazhou@gmail.com>
2016-06-12 00:45:46 -1000

    add container for all posts and style individual posts

--- js/app.js
+++ js/app.js
@@ -20,13 +20,27 @@ function display(obj) {
   $redditContainer.attr('id', 'reddit-container');
   $('body').append($redditContainer);
 
+  //reddit header container
+  var $redditHeader = $('<div />');
+  $redditHeader.attr('id', 'reddit-header');
+  var $h1Elem = $('<h1 />');
+  $h1Elem.text('Welcome to Corgiville!');
+  $redditHeader.append($h1Elem);
+  $redditContainer.append($redditHeader);
+
+  var $allPosts = $('<div />');
+  $allPosts.attr('id', 'all-posts');
+  $redditContainer.append($allPosts);
+
   var posts = obj.data.children;
   for (var i = 0; i < posts.length; i++) {
     //create div element for each post
     var $redditPost = $('<div />').addClass('reddit-post');
-    $redditContainer.append($redditPost);
+    $allPosts.append($redditPost);
 
     //url for img source or reddit post
+    var $redditPhoto = $('<div />').addClass('reddit-photo');
+    $redditPost.append($redditPhoto);
     var postLink = posts[i].data.url;
 
     //thumbnail photos with link to original photo (or reddit post)
@@ -48,44 +62,45 @@ function display(obj) {
     $imgLink.attr('href', postLink);
     $imgLink.attr('target', '_blank');
     $imgLink.append($imgElem);
-    $redditPost.append($imgLink);
+    $redditPhoto.append($imgLink);
+
+    //div for all text
+    var $redditText = $('<div />').addClass('reddit-text');
+    $redditPost.append($redditText);
 
     //reddit title
     var title = posts[i].data.title;
     if (title.length > 19){
       title = title.substring(0, 18) + '...';
     }
-    var $titleElem = $('<p />');
+    var $titleElem = $('<p />').addClass('title');
     var $titleLink = $('<a />');
-    $titleLink.addClass('title');
     $titleLink.attr('href', postLink);
     $titleLink.attr('target', '_blank');
     $titleLink.append(title);
     $titleElem.append($titleLink);
-    $redditPost.append($titleElem);
+    $redditText.append($titleElem);
 
     //reddit author
     var author = posts[i].data.author;
-    var $authorElem = $('<p />');
+    var $authorElem = $('<p />').addClass('author');
     var $authorLink = $('<a />');
-    $authorLink.addClass('author');
     $authorLink.attr('href', 'https://www.reddit.com/user/' +author);
     $authorLink.attr('target', '_blank');
     $authorLink.append(author);
     $authorElem.append($authorLink);
-    $redditPost.append($authorElem);
+    $redditText.append($authorElem);
 
     //reddit comments
     var commentUrl = 'https://www.reddit.com' + posts[i].data.permalink;
     var comments = posts[i].data.num_comments;
     var isPlural = (comments > 1 || comments === 0) ? ' comments' : ' comment';
-    var $commentElem = $('<p />');
+    var $commentElem = $('<p />').addClass('comment');
     var $commentLink = $('<a />');
-    $commentLink.addClass('comment');
     $commentLink.attr('href', commentUrl);
     $commentLink.attr('target', '_blank');
     $commentLink.append(comments + " " + isPlural);
     $commentElem.append($commentLink);
-    $redditPost.append($commentElem);
+    $redditText.append($commentElem);
   }
 }

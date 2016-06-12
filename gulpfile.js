31ec1fa0ae625ddecb1e4ec34e05f43b0cbf3568
Lisa <misslisazhou@gmail.com>
2016-06-11 14:11:41 -1000

    add watch task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
 
+gulp.task('watch', function () {
+  gulp.watch('app.js', function () {
+    console.log("The file changed.");
+  });
+});
+
 gulp.task('default', function () {
   console.log('hiiiiiiiii!');
 });
